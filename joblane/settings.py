d54cf84521f359678800379a03ecd3e3079de9b7
Vishal Singh <133119527+Vishal18cse@users.noreply.github.com>
2025-09-29 18:50:42 +0530

    using port 465

--- joblane/settings.py
+++ joblane/settings.py
@@ -175,8 +175,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+# EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("BREVO_EMAIL")        
 EMAIL_HOST_PASSWORD = os.getenv("BREVO_SMTP_KEY") 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
