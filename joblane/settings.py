ae6438c585be82b1cc16c114f4d5ae5033d62a98
Vishal Singh <133119527+Vishal18cse@users.noreply.github.com>
2025-09-29 18:21:32 +0530

    Brevo setup

--- joblane/settings.py
+++ joblane/settings.py
@@ -177,7 +177,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("BREVO_EMAIL")       # Your verified sender email
-EMAIL_HOST_PASSWORD = os.getenv("BREVO_SMTP_KEY") # Brevo SMTP key
-DEFAULT_FROM_EMAIL = os.getenv("BREVO_EMAIL")
+EMAIL_HOST_USER = os.getenv("BREVO_EMAIL")        
+EMAIL_HOST_PASSWORD = os.getenv("BREVO_SMTP_KEY") 
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 
